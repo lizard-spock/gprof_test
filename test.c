0eb668a008e0cf0d75b96e841ea497a3e4797c9d
Sarah <ww.sarah7150@gmail.com>
2024-07-16 01:26:33 +0000

    now workes but only have flat profile, not call graph. might bc i don't have mcleanup

--- test.c
+++ test.c
@@ -3,24 +3,36 @@
 #include <sys/gmon.h>
 
 void foo1() {
-    for (int i = 0; i < 1000000; ++i);
+    for (int i = 0; i < 10000000; ++i);
+}
+
+void foo3() {
+    for (int i = 0; i < 1000000000; ++i);
 }
 
 void foo2() {
-    for (int i = 0; i < 1000000; ++i);
+    for (int i = 0; i < 100000000; ++i);
+    foo3();
+}
+
+
+// Dummy function to mark the end of profiling
+void end_profiling() {
 }
 
 int main() {
     foo1();
     printf("Hello, profiling!\n");
 
-    // Start profiling
-    monstartup((unsigned long)&foo2, (unsigned long)(&foo2 + 1));
+    // Start profiling before the second call to foo2()
+    monstartup((unsigned long)&foo2, (unsigned long)&end_profiling);
 
-    foo2();  // This call to foo() will be profiled
+    foo2();  // This call to foo2() will be profiled
 
     // Stop profiling and cleanup
-    _mcleanup();
+    end_profiling();  // Mark the end of profiling
+
+    foo1();  // This call to foo1() will not be profiled
 
     return 0;
 }
